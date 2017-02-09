f5b624d5769985fca7f9f79db12d919d26d56134
茂木聡史 <motegisatoshi@motegisatoshis-MacBook-Air.local>
2017-02-09 21:08:37 +0900

    change file

--- index.php
+++ index.php
@@ -26,8 +26,9 @@ foreach ($events as $event) {
     error_log('Non text message has come');
     continue;
   }
-  replyImageMessage($bot,$event->getReplyToken(),"https://".$_SERVER["HTTP_HOST"].
-  "/imgs/original.jpg","https://".$_SERVER["HTTP_HOST"]."/imgs/preview.jpg");
+
+  replyLocationMessage($bot,$event->getReplyToken(),"LINE","東京都渋谷区渋谷2-21-1
+   ヒカリエ27階", 35.659025, 139.703473);)
 }
 
 
@@ -42,6 +43,14 @@ function replyTextMessage($bot,$replyToken,$text) {
 function replyImageMessage($bot,$replyToken,$originalImageUrl,$previewImageUrl) {
   $response = $bot->replyMessage($replyToken,new
   \LINE\LINEBot\MessageBuilder\ImageMessageBuilder($originalImageUrl,$previewImageUrl));
+  if (!$response->isSucceeded()) {
+    error_log('Failed!'.$response->getHTTPStatus.''.$response->getRawBody());
+  }
+}
+
+fuction replyLocationMessage($bot,$replyToken,$title,$address,$lat,$lon) {
+  $response = $bot->replyMessage($replyToken,new
+  \LINE\LINEBot\MessageBuilder\LocationMessageBuilder($title,$address,$lat,$lon));
   if (!$response->isSucceeded()) {
     error_log('Failed!'.$response->getHTTPStatus.''.$response->getRawBody());
   }
