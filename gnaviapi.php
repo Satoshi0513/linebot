8c41568fe7d4a0e6a6aba0c81bd652e4fadf8b73
Satoshi0513 <smotegi336@gmail.com>
2017-04-09 16:47:15 +0900

    change code in loop

--- gnaviapi.php
+++ gnaviapi.php
@@ -19,27 +19,21 @@ class Gnaviapi {
   						array('timeout'=>5)
   						);
   $context = stream_context_create($opts);
-  //Web APIへリクエスト送信
-  try{
+
     $json = file_get_contents($this->_build($lat,$lng),0,$context);
 
-    throw new Exception('Access Erorr gnavi',0);
-  } catch (Exception $e){
-    error_log($e->getMessage());
-  }
+    if ($json===FALSE) {
+      throw new Exception ('Access Error gnavi',0);
+    }
+
   //Web APIから取得したXMLデータをオブジェクトに代入
     $ret = json_decode($json);
 
 
   //Web APIのレスポンスがエラーか確認
-try{
-  if(isset($ret->error)){
-  throw new Exception('Gnaviapi Error',(int) $ret->error->code);
-}
-} catch(Exception $e){
-  error_log('Gnaviapi Resopnce Error:'.$ret->error->message);
-}
-
+  if(isset($json->error)) {
+    throw new Exception('Gnaciapi Error:'.$json->error->error_message);
+  }
 
 return $ret;
 
