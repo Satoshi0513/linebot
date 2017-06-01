24f6593648fc84b488ae4e4edb19aab7ee1146d3
Satoshi0513 <smotegi336@gmail.com>
2017-06-02 02:39:55 +0900

    change code

--- index.php
+++ index.php
@@ -41,7 +41,7 @@ foreach ($events as $event) {
   }
 
 if (isset($json)){
-  error_log("josn data".var_dump($json));
+  error_log("josn data".print_r($json));
 }
   $columnArray = array();
   $i = 0;
